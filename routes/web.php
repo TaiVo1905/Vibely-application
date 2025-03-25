f5951e440ca561760d9d6fabc6dc32200543deaf
Võ Thị Thu Hiền <hien.vo26@student.passerellesnumeriques.org>
2025-03-25 09:33:26 +0700

    VA-12/feat: create a header component

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('hello');
+    return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('Layouts.MainLayout');
+Route::get('/sidebar', function () {
+    return view('components.sidebar');
 });

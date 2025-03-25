bcb69dfc1dfb1bc576d4c0c8bdfe6e718c73f49d
binhnexusx <binhhoangdev9@gmail.com>
2025-03-25 09:10:06 +0700

    VA-12/feat: create a main layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 import './bootstrap';
+import '../css/MainLayout.css'
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hello');
 });
+
+Route::get('/test', function () {
+    return view('Layouts.MainLayout');
+});
